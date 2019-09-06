04329563bccf9793cf4ed98dc0a8131076ebac84
Nicholas Sosa <nicksosa456@gmail.com>
2019-09-06 12:39:37 -0500

    rolled dice. making fragments

---
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -0,0 +1,26 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RollDiceController {
+    @GetMapping("/roll-dice")
+    public String rollDice() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    @ResponseBody
+    public String rolledDice(@PathVariable int n, Model viewModel) {
+        int dice = (int) Math.floor(Math.random()+1);
+        if (n == dice) {
+            return "The number is "+dice+". You guessed "+n+". You guessed correctly~";
+        } else {
+            return "The number is "+dice+". You guessed "+n+". You guessed incorrectly.";
+        }
+    }
+}
