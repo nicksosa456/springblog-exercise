8747a9344bf805f8896838457482568784dc1a79
Nicholas Sosa <nicksosa456@gmail.com>
2019-09-06 14:25:05 -0500

    finished exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,6 +1,8 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.Models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +17,9 @@ public class PostController {
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String individualPost(@PathVariable int id) {
+    public String individualPost(@PathVariable int id, Model viewModel) {
+        Post post = new Post("Test Post", "First test post");
+        viewModel.addAttribute("id", post);
         return "posts/show";
     }
 
