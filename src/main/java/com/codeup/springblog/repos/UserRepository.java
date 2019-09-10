45a7d2cd29b16627e90c0f3b6cdc1291ee69d28d
Nicholas Sosa <nicksosa456@gmail.com>
2019-09-10 10:40:38 -0500

    made user repo

---
+++ src/main/java/com/codeup/springblog/repos/UserRepository.java
@@ -0,0 +1,9 @@
+package com.codeup.springblog.repos;
+
+import com.codeup.springblog.Models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
