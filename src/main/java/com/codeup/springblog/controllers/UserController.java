6d082cefbd225b89a4a1aaa0883f9b9e164ff7b9
Nicholas Sosa <nicksosa456@gmail.com>
2019-09-11 09:47:27 -0500

    turned login and register page to have form model binding

--- src/main/java/com/codeup/springblog/controllers/UserController.java
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.codeup.springblog.repos.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,35 +19,26 @@ public class UserController {
     }
 
     @GetMapping("/register")
-    public String viewRegister() {
+    public String viewRegister(Model model) {
+        model.addAttribute("user", new User());
         return "users/register";
     }
 
     @PostMapping("/register")
-    public String registerUser(
-            @RequestParam(name = "email") String email,
-            @RequestParam(name = "username") String username,
-            @RequestParam(name = "password") String password
-    ) {
-        User registerUser = new User();
-        registerUser.setEmail(email);
-        registerUser.setUsername(username);
-        registerUser.setPassword(password);
+    public String registerUser(@ModelAttribute User registerUser) {
         userDao.save(registerUser);
         return "redirect:/posts";
     }
 
     @GetMapping("/login")
-    public String viewLogin() {
+    public String viewLogin(Model model) {
+        model.addAttribute("user", new User());
         return "users/login";
     }
 
     @PostMapping("/login")
-    public String logUserIn(
-            @RequestParam(name = "username") String username,
-            @RequestParam(name = "password") String password
-    ) {
-        User loggedInUser = new User();
+    public String logUserIn(@ModelAttribute User loggedInUser) {
+
         return "redirect:/posts";
     }
 }
